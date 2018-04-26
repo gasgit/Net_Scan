40dbbb096093c52abadc92ecd97f9415fe17913f
gasgit <glengardiner@gmail.com>
2018-04-26 02:02:06 +0100

    inital commit, basic ping function

---
+++ ping.py
@@ -0,0 +1,26 @@
+
+import os
+
+# 
+ip_list = ["192.168.0.1", "192.168.0.108", "8.8.8.8"]
+
+
+# goal is to buils quick check tool to check status lan/wan 
+# devices up/down
+# early days
+
+
+
+def check_ping(hostname):
+    response = os.system("ping -c 1 " + hostname)
+    if response == 0:
+        status = "Device Online"
+    else:
+        status = "Device Error"
+
+    return status
+
+
+for hostname in ip_list:
+    check_ping(hostname)
+
