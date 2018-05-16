009571906eb9f633ebe1c8678f20e2a86d5e8e94
Glen Gardiner <a-ggardiner@COGALVEC.IE>
2018-05-16 17:41:14 +0100

    added landing, begin, rerun

--- gui.py
+++ gui.py
@@ -1,37 +1,41 @@
 from flask import Flask, render_template
+import os, socket, datetime
 app = Flask(__name__)
-import os, socket
 
 
-ip_list = {"Athenry HQ DC1":"001DC1", "Athenry HQ FP1":"001FP1", "Eachreidh DC1":"159DC1", "Eachreidh FP1":"159FP1", "Clairn DC":"015DC2", "Clairn FP":"015FP1",
-            "St Brigids DC":"007DC1", "St Brigids FP":"007FP1", "AL Loughrea DC":"069DC1", "AL Loughrea FP":"069FP1", "AL Portumna DC":"146DC1", "AL Portumna FP":"146FP1",
-            "YR Portumna DC":"203DC1", "YR Portumna FP":"203FP1", "St Killians DC": "009DC1", "St Killians FP":"009FP1", "AL Cliften DC":"149DC1", "AL Cliften FP": "149FP1"}
+
+ip_list = {"ATH HQ DC1":"001DC1", "ATH HQ FP1":"001FP1", "EH DC1":"159DC1", "EH FP1":"159FP1", "CLN DC":"015DC2", "CLN FP":"015FP1",
+            "BRD DC":"007DC1", "BRD FP":"007FP1", "AL LA DC":"069DC1", "AL LA FP":"069FP1", "AL PA DC":"146DC1", "AL PA FP":"146FP1",
+            "YR PA DC":"203DC1", "YR PA FP":"203FP1", "KN DC": "009DC1", "KN FP":"009FP1", "AL CTN DC":"149DC1", "AL CTN FP": "149FP1",
+             "VT GY FP":"247FP1", "VT GY DC":"247DC1"}
 
 
 status_collection = []
 
 
 class MyStatusObject:
-    def __init__(self, server_location, server_name, server_status, server_ip):
+    def __init__(self, server_location, server_name, server_status, server_ip, status_time):
         self.server_location = server_location
         self.server_name = server_name
         self.server_status = server_status
         self.server_ip = server_ip
+        self.status_time = status_time
 
     def __repr__(self):
-        return  "\nLN: " + self.server_location + "\nNM: " + self.server_name + "\nST: " + self.server_status + "\nIP: " + self.server_ip 
+        return  "\nLN: " + self.server_location + "\nNM: " + self.server_name + "\nST: " + self.server_status + "\nIP: " + self.server_ip + "\nTE: " + self.status_time
 
 
 def check_ping(k,v):
     ip = socket.gethostbyname(v)
-    response = os.system("ping -n 2 " + v)
+    response = os.system("ping -n 1 " + v)
+    stat_time = datetime.datetime.now()
     if response == 0: 
         status = "Device Online" 
-        my_new_object = MyStatusObject(k, v, status, ip )
+        my_new_object = MyStatusObject(k, v, status, ip, stat_time )
         status_collection.append(my_new_object)
     else:
         status = "Device Error"
-        my_new_object = MyStatusObject(k, v, status, ip )
+        my_new_object = MyStatusObject(k, v, status, ip, stat_time )
         status_collection.append(my_new_object)
     return status
 
@@ -44,33 +48,24 @@ def print_status_collection(col):
         print obj
 
 
-def rerun():
-    status_collection[:] = []
-    pass_iplist(ip_list)
+
+@app.route('/api/servers/')
+def index():
+    return render_template('index.html')
 
 
-
-# pass_iplist(ip_list)
-# print_status_collection(status_collection)
-
-
-@app.route("/api/servers")
-def hello():
+@app.route("/api/servers/display")
+def display_servers():
     pass_iplist(ip_list)
     print_status_collection(status_collection)
-
-
-
-    
-    return render_template('index.html', status_collection= status_collection)
+    return render_template('display_servers.html', status_collection= status_collection)
 
 
 @app.route('/api/servers/rerun')
 def rerun():
     status_collection[:] = []
     pass_iplist(ip_list)
-
-    return render_template('index.html', status_collection= status_collection)
+    return render_template('display_servers.html', status_collection= status_collection)
 
 if __name__ == '__main__':
     app.run()
