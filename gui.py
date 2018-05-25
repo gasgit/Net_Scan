1ab5e7baaa61059115ab3ba485fa45673de8fddd
Glen Gardiner <a-ggardiner@COGALVEC.IE>
2018-05-25 15:41:35 +0100

    crude refresh

--- gui.py
+++ gui.py
@@ -17,7 +17,7 @@ import pprint
 ip_list = {"ATH HQ DC1":"001DC1", "ATH HQ FP1":"001FP1", "EH DC1":"159DC1", "EH FP1":"159FP1", "CLN DC":"015DC2", "CLN FP":"015FP1",
             "BRD DC":"007DC1", "BRD FP":"007FP1", "AL LA DC":"069DC1", "AL LA FP":"069FP1", "AL PA DC":"146DC1", "AL PA FP":"146FP1",
             "YR PA DC":"203DC1", "YR PA FP":"203FP1", "KN DC": "009DC1", "KN FP":"009FP1", "AL CTN DC":"149DC1", "AL CTN FP": "149FP1",
-             "VT GY FP":"247FP1", "VT GY DC":"247DC1"}
+             "VT GY FP":"247FP1", "VT GY DC":"247DC1", "MY AL DC":"047DC1", "MY AL FP":"047FP1","OD DC":"106DC1", "OD FP":"106FP1" }
 
 # ip_list = {"CF1":"1.1.1.1","CF2":"1.0.0.1","GE1":"8.8.8.8","GE2":"8.8.4.4","Virgin":"virginmedia.ie", "Google":"google.ie", "Bing":"bing.ie",
 # "Python":"python.org"}
@@ -61,7 +61,7 @@ def check_ping(k,v):
         status_collection.append(my_new_object)
    
     else:
-        status = "Device Error"
+        status = "Error"
         my_new_object = MyStatusObject(k, v, status, ip, p, ttime)
         status_collection.append(my_new_object)
      
@@ -88,9 +88,9 @@ current_logs = []
 def read_logs():
      with open('my_logs.json') as f:
         data = json.load(f)
-        print data
-        for i in data:
-            print i
+        #print data
+        # for i in data:
+        #     print i
        
        
 
@@ -100,9 +100,6 @@ writelogs(status_collection)
 
 
 
-
-
-time.sleep(1)
 
 class App(Frame):
 
@@ -116,7 +113,9 @@ class App(Frame):
         
 
     def CreateUI(self):
+
         tv = Treeview(self)
+       
         
         tv['columns'] = ('servername','location', 'ip', 'status')
         
@@ -150,12 +149,25 @@ class App(Frame):
         for i in status_collection:
             self.treeview.insert('', 'end',text=i.test_time, values=(i.server_name,i.server_location, i.server_ip, i.server_status))
            
-       
-   
 
+  
 
 def main():
     root = Tk()
+
+    def refresh():
+        root.destroy()
+        execfile("gui.py")    
+
+    # create a toplevel menu
+    menubar = Menu(root)
+    menubar.add_command(label="Refresh!", command=refresh)
+    menubar.add_command(label="Quit!", command=root.quit)
+
+    # display the menu
+    root.config(menu=menubar)
+
+    scrollbar = Scrollbar(root)
     root.title("Ping Results")
     root.geometry('800x600')
     App(root)
